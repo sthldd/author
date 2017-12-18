da05a4e19a6e1a2da451a6c74f0871a684ce0972
mlx <1209624528@qq.com>
2017-12-18 15:30:32 +0800

    添加

---
+++ demo.sh
@@ -0,0 +1,18 @@
+if [ -d $1 ]; then
+  echo "$1 已经存在了"
+  exit
+else
+  mkdir $1
+  cd $1
+  mkdir css js
+  touch index.html css/style.css js/main.js
+  echo ' h1 {color:red;}'  >> css/style.css
+  echo ' <!DOCTYPE>
+  <title>Hello</title>
+  <h1>Hi</h1>
+  '  >> index.html
+  echo ' var string = "Hello World "
+  alert(string)
+  '  >> js/main.js
+  exit
+  fi
