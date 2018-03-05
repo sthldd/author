fe26a5f44612fad2e1ae1aa46c22cfa257c7fc8b
mlx <1209624528@qq.com>
2018-03-05 22:46:39 +0800

    更新代码

--- NtesMusic/song.js
+++ NtesMusic/song.js
@@ -15,7 +15,23 @@ $(function(){
      array.map(function(object){
        let $p = $('<p/>')
        $p.attr('data-time',object.time).text(object.words)
-       $p.appendTo($lyric)
-     })
+       $p.appendTo($lyric.children('.lines'))
+    })
   })
+
+  let audio = document.createElement('audio')
+  audio.src = 'http://dl.stream.qqmusic.qq.com/C400001XdWPc3zSK8E.m4a?vkey=A6D198708073FC36887509ABBE29EF0781F1A770540EA871A22025EA47FEC0A1A7225FB6C25BB5D667A51E921E13B8C0CE041CEF4CB0219D&guid=1559421735&uin=1209624528&fromtag=66'
+  audio.oncanplay = function(){
+    audio.play()
+    $('.disc-container').addClass('playing')
+  }
+  $('.icon-pause').on('click',function(){
+    audio.pause()
+    $('.disc-container').removeClass('playing')
+  })
+  $('.icon-play').on('click',function(){
+    audio.play()
+    $('.disc-container').addClass('playing')
+  }) 
+  
 })
