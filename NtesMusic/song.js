34e84e3d087a65bf5f18c16df90daceacc3753e5
mlx <1209624528@qq.com>
2018-03-07 14:42:39 +0800

    添加Code

--- NtesMusic/song.js
+++ NtesMusic/song.js
@@ -5,7 +5,16 @@ $(function(){
     console.log(songs)
     let song = songs.filter((s)=>{return s.id == id})[0]
     console.log(song)
-    let {url} = song 
+    let {url,name,lyric} = song 
+    console.log(url,name,lyric)
+    initPlayer.call(undefined,url)
+    initText(name,lyric)
+  })
+  function initText(name,lyric){
+    $('.son-description>h1').text(name)
+    parseLyric(lyric)
+  }
+  function initPlayer(url){
     let audio = document.createElement('audio')
     audio.src = url
     audio.oncanplay = function(){
@@ -20,28 +29,23 @@ $(function(){
       audio.play()
       $('.disc-container').addClass('playing')
     }) 
-
-  })
-  $.get('/lyric.json').then(function(object){
-     let {lyric} = object
-     let array = lyric.split('\n')
-     let regex = /^\[(.+)\](.*)$/
-     array = array.map(function(string,index){
-        let matches = string.match(regex)
-        if(matches){
-            return {time:matches[1],words:matches[2]}
-        }
-         
-     })
-     console.log(array)
-     let $lyric = $('.lyric')
-     array.map(function(object){
-       let $p = $('<p/>')
-       $p.attr('data-time',object.time).text(object.words)
-       $p.appendTo($lyric.children('.lines'))
+  }
+  function parseLyric(lyric){
+    let array = lyric.split('\n')
+    let regex = /^\[(.+)\](.*)$/
+    array = array.map(function(string,index){
+       let matches = string.match(regex)
+       if(matches){
+           return {time:matches[1],words:matches[2]}
+       }
+        
     })
-  })
-
-
-  
+    console.log(array)
+    let $lyric = $('.lyric')
+    array.map(function(object){
+      let $p = $('<p/>')
+      $p.attr('data-time',object.time).text(object.words)
+      $p.appendTo($lyric.children('.lines'))
+   })
+  }
 })
